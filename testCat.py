8304bd7796764fb5bf27b02f10e55f655b5238b2
Darishow <124413768+Darishow@users.noreply.github.com>
2023-05-08 07:02:56 -0300

    Create testCat.py

---
+++ testCat.py
@@ -0,0 +1,7 @@
+from cat import Cat
+
+cat_1 = Cat('Барон', 'мальчик', '2')
+cat_2 = Cat('Сэм', 'мальчик', '2')
+
+print(f'имя: {cat_1.name} - пол: {cat_1.gender} - возраст: {cat_1.age} года')
+print(f'имя: {cat_2.name} - пол: {cat_2.gender} - возраст: {cat_2.age} года')
